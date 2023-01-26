8ce5f187441da6e2ebf05dd66a30ce6ac484a83a
hhw12409 <sksk2425@naver.com>
2023-01-27 00:38:47 +0900

    feat: add maybePop

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,20 +7,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(title: 'Home Screen', children: [
-      ElevatedButton(
-        onPressed: () async {
-          final result = await Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (BuildContext context) => const RouteOneScreen(
-                number: 123,
+    return MainLayout(
+      title: 'Home Screen',
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            // 더 이상 뒤로 갈 페이지가 없을 때 maybePop은 뒤로 가지지 않는다.
+            Navigator.of(context).maybePop();
+          },
+          child: const Text('maybePop'),
+        ),
+        // []
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('pop'),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            final result = await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (BuildContext context) => const RouteOneScreen(
+                  number: 123,
+                ),
               ),
-            ),
-          );
-          print(result);
-        },
-        child: const Text('Push'),
-      ),
-    ]);
+            );
+          },
+          child: const Text('Push'),
+        ),
+      ],
+    );
   }
 }
