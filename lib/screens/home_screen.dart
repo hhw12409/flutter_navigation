259fff477263a4e39dae24192eaf46ffac308494
hhw12409 <sksk2425@naver.com>
2023-01-27 00:43:21 +0900

    feat: WillPopScope를 이용한 안드 자체 pop기능 막기

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,36 +7,51 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(
-      title: 'Home Screen',
-      children: [
-        ElevatedButton(
-          onPressed: () {
-            // 더 이상 뒤로 갈 페이지가 없을 때 maybePop은 뒤로 가지지 않는다.
-            Navigator.of(context).maybePop();
-          },
-          child: const Text('maybePop'),
-        ),
-        // []
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: const Text('pop'),
-        ),
-        ElevatedButton(
-          onPressed: () async {
-            final result = await Navigator.of(context).push(
-              MaterialPageRoute(
-                builder: (BuildContext context) => const RouteOneScreen(
-                  number: 123,
+    return WillPopScope(
+      onWillPop: () async {
+        // true - pop 가능
+        // false - pop 불가능
+        // 안드 자체에 있는 뒤로가기를 막을 수 있음.
+        return false;
+      },
+      child: MainLayout(
+        title: 'Home Screen',
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              // pop가능 여부를 boolean값으로 불러준다.
+              Navigator.of(context).canPop();
+            },
+            child: const Text('canPop'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // 더 이상 뒤로 갈 페이지가 없을 때 maybePop은 뒤로 가지지 않는다.
+              Navigator.of(context).maybePop();
+            },
+            child: const Text('maybePop'),
+          ),
+          // []
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('pop'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              final result = await Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (BuildContext context) => const RouteOneScreen(
+                    number: 123,
+                  ),
                 ),
-              ),
-            );
-          },
-          child: const Text('Push'),
-        ),
-      ],
+              );
+            },
+            child: const Text('Push'),
+          ),
+        ],
+      ),
     );
   }
 }
